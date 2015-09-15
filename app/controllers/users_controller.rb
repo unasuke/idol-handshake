d7aa7dbf5ab938fe79d418791f724aaa2feb81ff
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-09-15 23:21:57 +0900

    Create Users#update
    userの情報を更新するmethodを作成
    最低限の処理しか行っていないので不十分
    validationやbefore_actionなどが不足

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def update
+    @user = current_user
+    @profile = @user.profile
+
+    @profile.serial_code = params[:user][:profile][:serial_code]
+
+    @user.save!
+    @profile.save!
+
+    redirect_to root_path
+  end
+
 end
