670844ebaf1e26c779ee5152041480f7834c9595
ikegawa-koshi <ikegawa.koshi@gmail.com>
2015-09-17 15:29:02 +0900

    qrコードできたよ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,32 @@ class HomeController < ApplicationController
   end
 
   def qrcode
+    arr = [ current_user.profile.prev_handshake_all,
+            current_user.profile.prev_handshake_honoka,
+            current_user.profile.prev_handshake_kotori,
+            current_user.profile.prev_handshake_umi,
+            current_user.profile.prev_handshake_hanayo,
+            current_user.profile.prev_handshake_rin,
+            current_user.profile.prev_handshake_maki,
+            current_user.profile.prev_handshake_eri,
+            current_user.profile.prev_handshake_nico,
+            current_user.profile.prev_handshake_nozomi,
+            current_user.profile.next_handshake_all,
+            current_user.profile.next_handshake_honoka,
+            current_user.profile.next_handshake_kotori,
+            current_user.profile.next_handshake_umi,
+            current_user.profile.next_handshake_hanayo,
+            current_user.profile.next_handshake_rin,
+            current_user.profile.next_handshake_maki,
+            current_user.profile.next_handshake_eri,
+            current_user.profile.next_handshake_nico,
+            current_user.profile.next_handshake_nozomi  ]
+
+    @qrsrc = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl="
+    for num in 0..20 do
+        @qrsrc = @qrsrc + arr[num].to_s + ","
+    end
+
   end
 
   def input_serial
