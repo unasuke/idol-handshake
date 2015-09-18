be031b9adf806779a080955530a92d9b01b2e359
ikegawa-koshi <ikegawa.koshi@gmail.com>
2015-09-18 23:54:42 +0900

    qrコードの仕様変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,8 +13,8 @@ class HomeController < ApplicationController
                 current_user.profile.prev_handshake_rin,
                 current_user.profile.prev_handshake_maki,
                 current_user.profile.prev_handshake_eri,
-                current_user.profile.prev_handshake_nico,
                 current_user.profile.prev_handshake_nozomi,
+                current_user.profile.prev_handshake_nico,
                 current_user.profile.next_handshake_all,
                 current_user.profile.next_handshake_honoka,
                 current_user.profile.next_handshake_kotori,
@@ -23,12 +23,15 @@ class HomeController < ApplicationController
                 current_user.profile.next_handshake_rin,
                 current_user.profile.next_handshake_maki,
                 current_user.profile.next_handshake_eri,
-                current_user.profile.next_handshake_nico,
-                current_user.profile.next_handshake_nozomi  ]
+                current_user.profile.next_handshake_nozomi,
+                current_user.profile.next_handshake_nico  ]
 
         @qrsrc = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl="
         for num in 0..19 do
-            @qrsrc = @qrsrc + arr[num].to_s + ","
+            if arr[num] > 4
+                arr[num] = 4
+            end
+            @qrsrc = @qrsrc + arr[num].to_s
         end
     end
 
